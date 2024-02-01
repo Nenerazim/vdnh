29372a1cbe62f35fe5a86a592a83b3e2eb7ff690
kirill <gondor230@gmail.com>
2024-02-01 20:57:32 +0300

    TopHeader

--- src/ui/components/Billboard/Categories/index.vue
+++ src/ui/components/Billboard/Categories/index.vue
@@ -1,11 +1,77 @@
+<script setup lang="ts">
+import {mockCategories} from '@ui/components/Billboard/Categories/mock.js';
+
+const showAll = ref(false);
+const activeCategory = ref(mockCategories[0].id);
+const categories = computed(() => (showAll.value ? mockCategories : mockCategories.slice(0, 5)));
+</script>
+
 <template>
-  <div class="categories-list"></div>
+  <div class="categories-list">
+    <div
+      v-for="category in categories"
+      :key="category.id"
+      class="category"
+      :class="{active: category.id === activeCategory}"
+      @click="activeCategory = category.id"
+    >
+      <img :src="category.img" :alt="category.name" />
+      {{ category.name }}
+    </div>
+    <div class="show-more" :class="{active: showAll}" @click="showAll = !showAll">
+      <svg width="12" height="12">
+        <use href="/sprites/icons.svg#arrow-short" />
+      </svg>
+      {{ showAll ? 'Скрыть' : 'Еще' }}
+    </div>
+  </div>
 </template>
 
 <style lang="scss" scoped>
+@import '@ui/assets/sass/variables/colors';
+@import '@ui/assets/sass/variables/mixins';
+
 .categories-list {
-  margin: 25px 0;
   display: flex;
   gap: 8px;
+  flex-wrap: wrap;
+  margin: 25px 0;
+  .show-more {
+    @include font16();
+    color: #000;
+    display: flex;
+    align-items: center;
+    padding: 9px 12px;
+    gap: 8px;
+    height: 38px;
+    svg {
+      transition: 0.3s;
+      rotate: 180deg;
+    }
+    &.active svg {
+      rotate: 0deg;
+    }
+  }
+  .category {
+    border-radius: 16px;
+    border: 1px solid rgba(211, 17, 43, 0.4);
+    @include font16();
+    color: #000;
+    display: flex;
+    align-items: center;
+    padding: 9px 12px;
+    gap: 8px;
+    transition: 0.3s;
+    &.active {
+      color: $white;
+      background: $black;
+    }
+    img {
+      width: 18px;
+      height: 18px;
+      border-radius: 50%;
+      object-fit: cover;
+    }
+  }
 }
 </style>
--- src/ui/components/Header/TopHeader.vue
+++ src/ui/components/Header/TopHeader.vue
@@ -7,14 +7,89 @@
         </svg>
       </NuxtLink>
     </div>
+    <div class="right-side">
+      <form class="search">
+        <svg width="33" height="33">
+          <use href="/sprites/icons.svg#icon-search" />
+        </svg>
+        <div class="border"></div>
+        <input type="text" placeholder="поиск по местам, событиям, новостям, правилам" />
+      </form>
+      <div class="links">
+        <a href="/" class="cart">
+          <svg width="24" height="24">
+            <use href="/sprites/icons.svg#cart" />
+          </svg>
+        </a>
+        <a href="/" class="cart">
+          <svg width="24" height="24">
+            <use href="/sprites/icons.svg#lang" />
+          </svg>
+        </a>
+        <a href="/"> Войти </a>
+      </div>
+    </div>
   </div>
 </template>
 
+<script setup lang="ts"></script>
 <style lang="scss" scoped>
+@import '@ui/assets/sass/variables/colors';
+@import '@ui/assets/sass/variables/mixins';
+
 .top-header {
   margin-top: 15px;
   padding-inline: 56px;
   display: flex;
   align-items: center;
+  justify-content: space-between;
+  gap: 50px;
+  .right-side {
+    flex: 1;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    gap: 8px;
+    .search {
+      display: flex;
+      align-items: center;
+      max-width: 642px;
+      width: 100%;
+      .border {
+        margin-inline: 3px 10px;
+        width: 1px;
+        background: $red;
+        height: 26px;
+      }
+      input {
+        background: transparent;
+        border: transparent;
+        @include font18();
+        font-family: 'Intl', sans-serif;
+        width: 100%;
+        &:focus {
+          outline: none;
+        }
+        &::placeholder {
+          color: $lightGrey5;
+          @include font18();
+          font-family: 'Intl', sans-serif;
+        }
+      }
+    }
+    .links {
+      display: flex;
+      align-items: center;
+      gap: 8px;
+    }
+    a {
+      display: inline-flex;
+      color: $red;
+      text-decoration: none;
+      font-size: 15px;
+      font-weight: 500;
+      min-width: 80px;
+    }
+  }
 }
 </style>
